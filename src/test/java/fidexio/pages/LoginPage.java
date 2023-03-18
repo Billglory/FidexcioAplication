7febf07cb47d9c2265a15eade61f0951f19cd601
Bill Glory <billglory90@gmail.com>
2023-03-18 17:48:18 +0300

    Sprint2 FidexioAplication

--- src/test/java/fidexio/pages/LoginPage.java
+++ src/test/java/fidexio/pages/LoginPage.java
@@ -1,28 +1,35 @@
 package fidexio.pages;
 
+import fidexio.utilities.ConfigurationReader;
 import fidexio.utilities.Driver;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 public class LoginPage {
 
-    public LoginPage(){
-        PageFactory.initElements(Driver.getDriver(),this);
+
+ public LoginPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(xpath = "//*[@id='login']")
-    public WebElement inputEmail;
 
+    @FindBy(id = "login")
+    public WebElement inputUsername;
+
+    @FindBy(id = "password")
+    public WebElement inputPassword;
 
     @FindBy(xpath = "//button[.='Log in']")
     public WebElement loginButton;
 
-    @FindBy(xpath = "//*[@id='password']")
-    public WebElement inputPassword;
+    @FindBy(xpath = "//p[@class='alert alert-danger']")
+    public WebElement wrongAlertMessage;
 
-    @FindBy(css = "p.alert.alert-danger")
-    public WebElement error;
+    public void loginMethod() {
+        inputUsername.sendKeys(ConfigurationReader.getProperty("username"));
+        inputPassword.sendKeys(ConfigurationReader.getProperty("password"));
+        loginButton.click();
 
-
-
+}
 }
--- src/test/java/fidexio/step_definitions/Login_Step_Definition.java
+++ src/test/java/fidexio/step_definitions/Login_Step_Definition.java
@@ -15,6 +15,140 @@ import io.cucumber.java.en.And;
 
 public class Login_Step_Definition {
 
+    LoginPage loginPage = new LoginPage();
+
+@Given("user goes to login page")
+public void user_goes_to_login_page() {
+  Driver.getDriver().get(ConfigurationReader.getProperty("env"));
 
 
+}
+
+    @When("{string} enter username ")
+    public void enter_username_and_password(String string, String userName) {
+   loginPage.inputUsername.sendKeys(userName);
+
+
+}
+
+    @Then("user enters password")
+    public void user_enters_password(String string, String password) {
+        loginPage.inputPassword.sendKeys(password);
+
+    }
+
+
+ @When("user click on login button")
+    public void user_click_on_login_button() {
+ loginPage.loginButton.click();
+ }
+
+@Then("user  clicks login button")
+public void user_clicks_login_button() {
+
+}
+
+@Then("user logged in")
+public void user_logged_in(io.cucumber.datatable.DataTable dataTable) {
+  BrowserUtils.verifyTitle("#Inbox - Odoo");
+
+
+}
+
+
+    @And("user enters invalid <password>")
+    public void userEntersInvalidPassword() {
+    }
+
+    @Given("user is on the login page")
+    public void userIsOnTheLoginPage() {
+    }
+
+    @Then("user should see discuss page ")
+    public void userShouldSeeDiscussPage(String arg0) throws Throwable {
+
+    }
+
+    @When("user enters invalid <username>")
+    public void userEntersInvalidUsername() {
+    }
+
+    @And("user clicks on the log in button")
+    public void userClicksOnTheLogInButton() {
+    }
+
+    @Then("user cannot login")
+    public void userCannotLogin() {
+    }
+
+    @When("user enters empty <username>")
+    public void user_Enters_Empty_Username() {
+    }
+
+    @Then("user should see the error message")
+    public void userShouldSeeTheErrorMessage() {
+    }
+
+    @When("user enters  <username>")
+    public void user_Enters_Username() {
+    }
+
+    @And("user enters <password>")
+    public void user_Enters_Password() {
+    }
+
+    @Then("user should not be able to see the password")
+    public void userShouldNotBeAbleToSeeThePassword() {
+    }
+
+    @And("user clicks on the enter key")
+    public void userClicksOnTheEnterKey() {
+    }
+
+    @Then("user logged in the home page")
+    public void userLoggedInTheHomePage() {
+    }
+
+    @Then("user should see discuss page")
+    public void user_Should_See_DiscussPage(String arg0) throws Throwable {
+
+    }
+
+    @When("user enters invalid <username")
+    public void user_Enters_Invalid_Username() {
+    }
+
+    @When("user enters empty <username")
+    public void userEntersEmptyUsername() {
+    }
+
+
+    @When("^user enters wrong username$")
+    public void userEntersWrongUsername() {
+    }
+
+    @Then("^user enters wrong password$")
+    public void userEntersWrongPassword() {
+    }
+
+    @And("^user clicks login button$")
+    public void userClicksLoginButton() {
+    }
+
+    @Then("^user cannot login warning$")
+    public void userCannotLoginWarning() {
+    }
+
+    @And("^user enters  <password>$")
+    public void user_EntersPassword() {
+    }
+
+    @When("user enters  <username>")
+    public void user_EntersUsername() {
+    }
+
+
+    @And("user enters <password>")
+    public void userEntersPassword() {
+    }
 }
